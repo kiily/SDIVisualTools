b6c2a7d357b3e1d8b6b1105de6e55f545a8dd894
Miguel Marin <miguelmarin95@hotmail.com>
2017-08-11 22:19:36 +0100

    Firebase connection (#12)
    * Scaffolding (#11)
    
    * Added link to access full dashboard
    
    works locally - need to test from a remote connection
    
    * Honeycomb menu (#7)
    
    * fixed honeycomb menu for home page
    
    * honey-comb fully responsive
    
    * added sacffolding buttons and relevant iframes with BI reports
    
    * Added card elements in about (#10)
    
    * Changed background to black
    
    Fixed the navbar to be white and changed the font
    
    * implemented signup form, sign up and login (email and password) with firebase; installed dependencies
    
    * added password reset, honeycomb animation, design attempt on login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { AuthService } from './services/auth.service';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SEATService } from './services/seat.service';
-import {PowerBIService} from './services/power-bi.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -14,6 +14,19 @@ import { InnovationComponent } from './innovation/innovation.component';
 import { HomeComponent } from './home/home.component';
 import { PyramidComponent } from './pyramid/pyramid.component';
 import { HexagonMenuComponent } from './hexagon-menu/hexagon-menu.component';
+import { AngularFireModule} from 'angularfire2';
+
+
+
+//Firebase database configuration settings
+export const firebaseConfig = {
+   apiKey: "AIzaSyDpKv2ZSRCffWFSua8lgy2YLVwTSHo_tRE",
+    authDomain: "sdi-visual-tools.firebaseapp.com",
+    databaseURL: "https://sdi-visual-tools.firebaseio.com",
+    projectId: "sdi-visual-tools",
+    storageBucket: "sdi-visual-tools.appspot.com",
+    messagingSenderId: "759960395397"
+}
 
 @NgModule({
   declarations: [
@@ -39,11 +52,13 @@ import { HexagonMenuComponent } from './hexagon-menu/hexagon-menu.component';
      {path: 'innovation', component: InnovationComponent},
      {path: 'home', component: HomeComponent},
      {path: 'welcome-page', component: WelcomePageComponent}
-    ])
+    ]),
+   AngularFireModule.initializeApp(firebaseConfig),
+ 
   ],
   providers: [
     SEATService,
-    PowerBIService
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,7 +1,8 @@
+import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { SignInValidators } from './sign-in.validators';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as pbi from 'powerbi-client';
 
 @Component({
@@ -10,34 +11,127 @@ import * as pbi from 'powerbi-client';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
-  
-  form = new FormGroup({
-    username: new FormControl('',[Validators.required, 
-      Validators.minLength(3),
-      SignInValidators.cannotContainSpace], 
-      SignInValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
-  });
 
-  constructor(private router : Router) {  }
+  // loginForm = new FormGroup({
+  //   email: new FormControl('', Validators.required),
+  //   password: new FormControl('', Validators.required)
+  // });
+
+  loginForm;
+  signUpForm;
+
+  // signupForm = new FormGroup({
+  //   email: new FormControl('', [Validators.required,
+  //   Validators.minLength(6),
+  //   SignInValidators.cannotContainSpace],
+  //     SignInValidators.shouldBeUnique),
+  //   password: new FormControl('', Validators.required),
+  //   repeatPassword: new FormControl('', Validators.required),
+  //   firstName: new FormControl('', Validators.required),
+  //   lastName: new FormControl('', Validators.required)
+
+  // });
+
+  constructor(private router: Router, private formBuilder: FormBuilder,
+    private authService: AuthService) {
+
+    this.loginForm = formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+
+    this.signUpForm = formBuilder.group({
+      email: ["", Validators.required],
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      password: ["", Validators.required],
+      repeatPassword: ["", Validators.required]
+
+    });
+  }
 
   ngOnInit() {
   }
 
-  login(){
-       //set errors at the form level (can call at individual
-      //form control object too)
-      this.form.setErrors({
-        invalidLogin: true
+  login() {
+
+    let email = this.loginForm.controls.email.value;
+    let password = this.loginForm.controls.password.value;
+
+    //Catch the loginPromise
+    this.authService.login(email, password)
+      .then(authState => {
+
+        //check whether email is verified
+        if (authState.auth.emailVerified) {
+          this.loginForm.reset();
+          //If everything is okay then navigate to home page
+          this.router.navigate(['/home']);
+        } else {
+          console.log("email is not verified");
+        }
+
+      }).catch(error => {
+        console.log(error);
+
       });
 
-      this.router.navigate(['/home']);
-    }
-  
+    //  //set errors at the form level (can call at individual
+    // //form control object too)
+    // this.loginForm.setErrors({
+    //   invalidLogin: true
+    // });
+  }
 
+
+  signUp() {
+
+    //retrieve data from the form
+    let email = this.signUpForm.controls.email.value;
+    let password = this.signUpForm.controls.password.value;
+    let repeatPassword = this.signUpForm.controls.repeatPassword.value;
+    let firstName = this.signUpForm.controls.firstName.value;
+    let lastName = this.signUpForm.controls.lastName.value;
+
+    console.log(email);
+    //check whether the passwords match
+    if (password == repeatPassword) {
+      //Catch the signup promise
+      this.authService.signupUser(email, password)
+        .then(authState => {
+
+          console.log("sending email");
+          //Email verification
+          authState.auth.sendEmailVerification();
+
+          this.signUpForm.reset();
+
+          //Extract unique user id
+          let uid = authState.uid;
+          this.authService.registerUser(uid, email, firstName, lastName);
+
+          //NAVIGATION (if any) SHOULD BE ADDED HERE
+        }).catch(error => {
+          console.log(error);
+        });
+
+    }
+  }
+
+
+  resetPassword(){
+   let email = this.loginForm.controls.email.value;
+   this.authService.resetPassword(email)
+   .then(() => {
+     console.log("emairu sent");
+   })
+    .catch( error =>{
+      console.log(error);
+    });
+  }
   //create a property (getter) this is similar to a method
-  get username(){
-    return this.form.get('username');
+  get username() {
+    return this.loginForm.get('username');
   }
 
 }
