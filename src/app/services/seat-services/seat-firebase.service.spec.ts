bd704ead40cf8ac3e03f79672a26653480498dd5
Miguel Marin <miguelmarin95@hotmail.com>
2017-09-08 00:05:32 +0100

    Final changes (#28)
    * styled the add data page for a better layout of everything, backbone for adding excel sheets
    
    * minor changes to the design of the scaffolding and add scaffolding data pages, updated version of the reports was included
    
    * updated small things and added favicon, changed fullscreen issue for report 3
    
    * reverted fullscreen for azure build
    
    * minor fixes to tests, fixes to design, added dean reference, minr fixes for cleaner logout btn, improve GUI of scaffolding page

--- src/app/services/seat-services/seat-firebase.service.spec.ts
+++ src/app/services/seat-services/seat-firebase.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { SEATFirebaseService } from './seatfirebase.service';
+import { SEATFirebaseService } from './seat-firebase.service';
 
 describe('SEATFirebaseService', () => {
   beforeEach(() => {
